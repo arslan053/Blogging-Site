394b7540722ff908467d6a975a190ec1356cd484
Muhammad Arslan <arslan05342@gmail.com>
2023-06-19 18:10:19 +0500

    User validations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
  class User < ApplicationRecord
-     
- end
+    validates :username, presence: true, 
+                     uniqueness: { case_sensitive: false }, 
+                     length: { minimum: 5, maximum: 27 }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true,  
+                  length: { maximum: 130}, 
+                  format: { with: VALID_EMAIL_REGEX }
+ end   
